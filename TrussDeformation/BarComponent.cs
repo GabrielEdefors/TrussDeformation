8bdc4160d9a93e03f1fe166961d647e6fc832b10
Gabriel Edefors <gabriel_edefors@hotmail.com>
2020-08-26 10:52:37 +0200

    Add project files.

---
+++ TrussDeformation/BarComponent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace TrussDeformation
+{
+	public class BarComponent : GH_Component
+	{
+		/// <summary>
+		/// Initializes a new instance of the MyComponent1 class.
+		/// </summary>
+		public BarComponent()
+		  : base("Bar", "Bar",
+			  "Bar Object",
+			  "Truss", "Elements")
+		{
+		}
+
+		/// <summary>
+		/// Registers all the input parameters for this component.
+		/// </summary>
+		protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+		{
+			pManager.AddPointParameter("Point 1", "p1", "First point of the bar", GH_ParamAccess.list);
+			pManager.AddPointParameter("Point 2", "p2", "Second point of the bar", GH_ParamAccess.list);
+			pManager.AddNumberParameter("Area", "A", "Cross sectional area of the bar", GH_ParamAccess.list);
+			pManager.AddNumberParameter("Youngs Modulus", "E", "Stiffness of the bar", GH_ParamAccess.list);
+		}
+
+		/// <summary>
+		/// Registers all the output parameters for this component.
+		/// </summary>
+		protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+		{
+			pManager.AddLineParameter("Line", "Line", "Line representing the geometry of the bar", GH_ParamAccess.list);
+		}
+
+		/// <summary>
+		/// This is the method that actually does the work.
+		/// </summary>
+		/// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+		protected override void SolveInstance(IGH_DataAccess DA)
+		{
+
+			// Retrive data from component
+			List<Point3d> point1 = new List<Point3d>();
+			List<Point3d> point2 = new List<Point3d>();
+			List<double> A = new List<double>();
+			List<double> E = new List<double>();
+
+			DA.GetDataList("Point 1", point1);
+			DA.GetDataList("Point 2", point2);
+			DA.GetDataList("Area", A);
+			DA.GetDataList("Youngs Modulus", E);
+
+		}
+
+		/// <summary>
+		/// Provides an Icon for the component.
+		/// </summary>
+		protected override System.Drawing.Bitmap Icon
+		{
+			get
+			{
+				//You can add image files to your project resources and access them like this:
+				// return Resources.IconForThisComponent;
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Gets the unique ID for this component. Do not change this ID after release.
+		/// </summary>
+		public override Guid ComponentGuid
+		{
+			get { return new Guid("289c727c-39fc-438e-81a3-358ae046e63b"); }
+		}
+	}
+}
---
+++ TrussDeformation/TrussDeformationInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace TrussDeformation
+{
+	public class TrussDeformationInfo : GH_AssemblyInfo
+	{
+		public override string Name
+		{
+			get
+			{
+				return "TrussDeformation";
+			}
+		}
+		public override Bitmap Icon
+		{
+			get
+			{
+				//Return a 24x24 pixel bitmap to represent this GHA library.
+				return null;
+			}
+		}
+		public override string Description
+		{
+			get
+			{
+				//Return a short string describing the purpose of this GHA library.
+				return "";
+			}
+		}
+		public override Guid Id
+		{
+			get
+			{
+				return new Guid("fbb869f2-4ce3-4d67-b932-ea012dacc3df");
+			}
+		}
+
+		public override string AuthorName
+		{
+			get
+			{
+				//Return a string identifying you or your company.
+				return "";
+			}
+		}
+		public override string AuthorContact
+		{
+			get
+			{
+				//Return a string representing your preferred contact details.
+				return "";
+			}
+		}
+	}
+}
